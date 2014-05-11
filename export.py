f6db51694dc4df6a2bc71abb6d58622bee9527cb
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-05-11 03:29:03 -0700

    Add simple export script to dump records as JSON

---
+++ export.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import json
+import sys
+
+with open(sys.argv[1]) as f:
+	records = [[p[0], p[1]] for p in (line.strip().split("\t") for line in f)]
+	print(json.dumps(records))
