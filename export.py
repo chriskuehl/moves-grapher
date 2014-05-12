76c26f69c5c7dde3b18e18a4b023c66482086a07
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-05-11 17:46:48 -0700

    Convert miles to float in export

--- export.py
+++ export.py
@@ -3,5 +3,5 @@ import json
 import sys
 
 with open(sys.argv[1]) as f:
-	records = [[p[0], p[1]] for p in (line.strip().split("\t") for line in f)]
+	records = [[p[0], float(p[1])] for p in (line.strip().split("\t") for line in f)]
 	print(json.dumps(records))
